476ce4217ade29ab794557fc193b4e842dd59692
zhouhao <zh.sqy@qq.com>
2021-02-09 16:57:09 +0800

    Deprecated

---
+++ rule-engine-api/src/main/java/org/jetlinks/rule/engine/api/model/NodeType.java
@@ -0,0 +1,33 @@
+package org.jetlinks.rule.engine.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.hswebframework.web.dict.EnumDict;
+
+/**
+ * @author zhouhao
+ * @since 1.0.0
+ */
+@Getter
+@AllArgsConstructor
+@Deprecated
+public enum NodeType implements EnumDict<String> {
+
+    //转换新的数据
+    MAP("MAP", true),
+    //仅消费数据
+    PEEK("PEEK", false);
+
+    private String text;
+    private boolean returnNewValue;
+
+    @Override
+    public String getValue() {
+        return name();
+    }
+
+    @Override
+    public boolean isWriteJSONObjectEnabled() {
+        return false;
+    }
+}
